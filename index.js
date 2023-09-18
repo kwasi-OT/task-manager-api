2df801a433c0c6c3bcdf65e37da09956ae6f9357
Kwasi O-T <tweneboah21@gmail.com>
2023-09-18 23:57:29 +0000

    defined data model

--- index.js
+++ index.js
@@ -7,3 +7,12 @@ const port = process.env.PORT || 4000;
 // define the middleware
 app.use(bodyParser.json());
 app.use(cors());
+
+// start the server
+app.listen(port, () => {
+    console.log(`Server is running on port ${port}`);
+});
+
+// Data model: array to store data for simplicity
+let tasks = [];
+let taskIdCounter = 1;
