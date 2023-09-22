50199927709ac6af12948785376463ca6afe33b7
Kwasi O-T <tweneboah21@gmail.com>
2023-09-22 17:43:59 +0000

    commented out cors

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const app = express();
 const bodyParser = require('body-parser');
-const cors = require('cors');
+// const cors = require('cors');
 const {db, pgp} = require('./db'); //import PostgreSQL configuration
 const port = process.env.PORT || 4000;
 
