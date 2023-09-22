c5917cf6bb59309fcc477721e017244c763c46cd
Kwasi O-T <tweneboah21@gmail.com>
2023-09-22 15:01:25 +0000

    updated connection link

--- db.js
+++ db.js
@@ -1,5 +1,5 @@
 const pgp = require('pg-promise')();
-const connectionString = 'postgres://your-username:your-password@localhost:5432/task_manager'; // Replace with your PostgreSQL connection details
+const connectionString = "postgres://default:aZEMy1H4gwQL@ep-shiny-resonance-58733223.us-east-1.postgres.vercel-storage.com:5432/verceldb"; // Replace with your PostgreSQL connection details
 const db = pgp(connectionString);
 
 module.exports = { db, pgp };
