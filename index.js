1c14ae37d4651cf379ce5439a00403187c6ec57e
Kwasi O-T <tweneboah21@gmail.com>
2023-09-22 13:18:52 +0000

    updated all endpoints

--- index.js
+++ index.js
@@ -2,6 +2,7 @@ const express = require('express');
 const app = express();
 const bodyParser = require('body-parser');
 const cors = require('cors');
+const {db, pgp} = require('./db'); //import PostgreSQL configuration
 const port = process.env.PORT || 4000;
 
 // define the middleware
@@ -18,8 +19,8 @@ app.listen(port, () => {
 });
 
 // Data model: array to store data for simplicity
-let taskIdCounter = 1;
-let tasks = [];
+// let taskIdCounter = 1;
+// let tasks = [];
 
 
 ///////////////
@@ -27,56 +28,93 @@ let tasks = [];
 ///////////////
 
 // get all tasks
-app.get('/tasks', (req, res) => {
+app.get('/tasks', async (req, res) => {
+    try {
+        const tasks = await db.any('SELECT * FROM tasks')
     res.json(tasks);
+    } catch (error) {
+        console.error('Error fetching tasks:', error);
+        res.status(500).json({error: 'Internal Server Error'});
+    }
 });
 
 // add a new task
-app.post('/tasks', (req, res) => {
-    const newTask = {
-        id: taskIdCounter++,
-        title: req.body.title,
-        description: req.body.description,
-        completed: false,
-    };
+app.post('/tasks', async (req, res) => {
+    try {
+        const newTask = {
+            title: req.body.title,
+            description: req.body.description,
+            completed: false,
+        };
 
-    tasks.push(newTask);
-    res.json(newTask);
+        const result = await db.one(
+            'INSERT INTO tasks (title, description, completed) VALUES (${title}, ${description}, ${completed}) RETURNING id',
+            newTask
+        );
+
+        newTask.id = result.id;
+        res.json(newTask);
+    } catch (error) {
+        console.error('Error adding task:', error);
+        res.status(500).json({error: 'Internal Server Error'});
+    }
 });
 
 // Update a task
-app.put('/tasks/:id', (req, res) => {
-    const taskId = parseInt(req.params.id);
-    const taskToUpdate = tasks.find((task) => task.id === taskId);
+app.put('/tasks/:id', async (req, res) => {
+    try {
+        const taskId = parseInt(req.params.id);
+        const taskToUpdate = await db.oneOrNone('SELECT * FROM tasks WHERE id = $1', taskId);
 
-    if (!taskToUpdate) {
-        return res.status(404).json({ error: 'Task not found' });
+        if (!taskToUpdate) {
+            return res.status(404).json({ error: 'Task not found' });
+        }
+
+        taskToUpdate.title = req.body.title || taskToUpdate.title;
+        taskToUpdate.description = req.body.description || taskToUpdate.description;
+
+        await db.none('UPDATE tasks SET title = $1, description = $2 WHERE id = $3', [
+            taskToUpdate.title,
+            taskToUpdate.description,
+            taskId,
+        ]);
+
+        res.json(taskToUpdate);
+    } catch (error) {
+        console.error('Error updating task:', error);
+        res.status(500).json({error: 'Internal Server Error'});
     }
-
-    taskToUpdate.title = req.body.title || taskToUpdate.title;
-    taskToUpdate.description = req.body.description || taskToUpdate.description;
-
-    res.json(taskToUpdate);
 });
 
 // Delete a task
-app.delete('/tasks/:id', (req, res) => {
+app.delete('/tasks/:id', async (req, res) => {
+    try {
     const taskId = parseInt(req.params.id);
-    tasks = tasks.filter((task) => task.id !== taskId);
+    await db.none('DELETE FROM tasks WHERE id = $1', taskId);
     res.json({ message: 'Task deleted successfully' });
+    } catch (error) {
+        console.error('Error deleting task:', error);
+        res.status(500).json({ error: 'Internal Server Error' });
+    }
 });
 
   // Mark a task as complete
-app.put('/tasks/:id/complete', (req, res) => {
-    const taskId = parseInt(req.params.id);
-    const taskToComplete = tasks.find((task) => task.id === taskId);
+app.put('/tasks/:id/complete', async (req, res) => {
+    try {
+        const taskId = parseInt(req.params.id);
+        const taskToComplete = await db.oneOrNone('SELECT * FROM tasks WHERE id = $1', taskId);
 
-    if (!taskToComplete) {
-        return res.status(404).json({ error: 'Task not found' });
+        if (!taskToComplete) {
+            return res.status(404).json({ error: 'Task not found' });
+        }
+
+        taskToComplete.completed = true;
+        await db.none('UPDATE tasks SET completed = $1 WHERE id = $2', [taskToComplete.completed, taskId]);
+        res.json(taskToComplete);
+    } catch (error) {
+        console.error('Error marking task as complete:', error);
+        res.status(500).json({ error: 'Internal Server Error' });
     }
-
-    taskToComplete.completed = true;
-    res.json(taskToComplete);
 });
 
 // end of endpoints
@@ -85,6 +123,3 @@ app.put('/tasks/:id/complete', (req, res) => {
 app.listen(port, () => {
     console.log(`Server is running on port ${port}`);
 });
-
-// Export the Express API
-module.exports = app
